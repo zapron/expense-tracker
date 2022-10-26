7050707966d485c1dc0b76df4d4c6f32569fec15
raktim-sg <raktim24@gmail.com>
2022-10-26 21:43:40 +0530

    Project setup
    Initial Files created

---
+++ screens/RecentExpenses.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Recentexpenses = () => {
+  return (
+    <View>
+      <Text>Recentexpenses</Text>
+    </View>
+  )
+}
+
+export default Recentexpenses
+
+const styles = StyleSheet.create({})
